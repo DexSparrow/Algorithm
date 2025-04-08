54f2d7ff01dded9c0b3d6095a7c55f154a2742fc
DexSparrow <dexsparrow@gmail.com>
2025-04-08 22:53:44 +0300

    The direction change should be len-s and not len-s-1 because the mirror teleportaion have distance too , [0,1,2] -> moving 0 to the tail have 1 movement cost

--- matrix_layer_rotation/solve.cpp
+++ matrix_layer_rotation/solve.cpp
@@ -19,7 +19,7 @@ void shift(vector <int*> & v,int s,int dir = 1){ // I used this function that I 
     s = s%len;
     int sens = (s>int(len/2)? -1 : 1); // invert the direction or not
     if(sens<0){
-        s = len-1-s;
+        s = len-s;
     }
     dir *= sens;
     vector <int> brouillon(s); // for storing variable that will be used for allocating and so we can move with iteration
@@ -105,7 +105,7 @@ void matrixRotation(vector<vector<int>>& matrix, int rotate) {
 void desc(vector <vector <int>> & V){ // displaying the matrix i used alphabetical translation for displaying cause number with more than 1 digit will make the visualizing dirty :(
     for(auto&x: V){
         for(auto&i: x)cout << char(i+97-1) << " ";
-        cout <<endl;
+        cout<<endl;
     }
 }
 
@@ -115,8 +115,20 @@ int main(){
         {6,7,8,9,10},
         {11,12,13,14,15},
         {16,17,18,19,20}
+/*
+    {9718805, 60013003, 5103628, 85388216, 21884498, 38021292, 73470430, 31785927},
+{69999937, 71783860, 10329789, 96382322, 71055337, 30247265, 96087879, 93754371},
+{79943507, 75398396, 38446081, 34699742, 1408833, 51189, 17741775, 53195748},
+{79354991, 26629304, 86523163, 67042516, 54688734, 54630910, 6967117, 90198864},
+{84146680, 27762534, 6331115, 5932542, 29446517, 15654690, 92837327, 91644840},
+{58623600, 69622764, 2218936, 58592832, 49558405, 17112485, 38615864, 32720798},
+{49469904, 5270000, 32589026, 56425665, 23544383, 90502426, 63729346, 35319547},
+{20888810, 97945481, 85669747, 88915819, 96642353, 42430633, 47265349, 89653362},
+{55349226, 10844931, 25289229, 90786953, 22590518, 54702481, 71197978, 50410021},
+{9392211, 31297360, 27353496, 56239301, 7071172, 61983443, 86544343, 43779176},
+*/
     });
-    int rotate = 3;
+    int rotate = 3;//40
     desc(V);cout<<"----X----"<<endl;
     matrixRotation(V, rotate);
     desc(V);cout<<"----X----"<<endl;
